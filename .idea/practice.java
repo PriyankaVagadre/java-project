23b16a93f3ba9e87bf6cd6493f1a926241f60c2f
priyankavagadre <priyankavagadre@hotmail.com>
2026-05-26 14:06:35 +0530

    ternary operator

--- .idea/practice.java
+++ .idea/practice.java
@@ -1,12 +1,7 @@
 class Scratch {
     public static void main(String[] args) {
        int salary = 20000;
-        String className ="Economy";
-                if(salary>=18000){
-                    className="First";
-                }
-        System.out.println("Customer in" + className + "Class");
-
-
+        String className = (salary>=18000) ? "First" : "Economy";
+        System.out.println("Customer in " + className + " Class");
     }
 }
