f0955ce7600eda56df270c83e9ac548893f58ce7
priyankavagadre <priyankavagadre@hotmail.com>
2026-05-26 13:54:31 +0530

    conditional statements

--- .idea/practice.java
+++ .idea/practice.java
@@ -1,6 +1,13 @@
 class Scratch {
     public static void main(String[] args) {
-        final float pI = 3.14F;
-
+        int temp =26;
+        if(temp>30){
+            System.out.println("Its hot today");
+        }
+        else if(temp>25 && temp<30){
+            System.out.println("Its a normal dat today");
+        }
+        else
+            System.out.println("Its a cold day today");
     }
 }
