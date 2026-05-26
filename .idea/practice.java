77c916789d86297d7abc5614dd54f61fdfbe3b1b
priyankavagadre <priyankavagadre@hotmail.com>
2026-05-26 15:56:37 +0530

    for Loop

--- .idea/practice.java
+++ .idea/practice.java
@@ -2,20 +2,8 @@ import java.util.Scanner;
 
 class Scratch {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number");
-        int numDiv = scanner.nextInt();
-        if(numDiv % 5 == 0 && numDiv % 3 == 0){
-            System.out.println("Fuzz Buzz");
-        }
-        else if(numDiv % 5 == 0){
-            System.out.println("Fuzz");
-        }
-        else if(numDiv % 3 == 0){
-            System.out.println("Buzz");
-        }
-        else{
-            System.out.println(numDiv);
+        for(int i=0;i<5;i++){
+            System.out.println("*");
         }
     }
 }
